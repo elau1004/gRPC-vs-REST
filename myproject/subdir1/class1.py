bfad4f97b7eed9122efb3f6bca642f4300435112
Edward Lau <elau1004@netscape.net>
2023-06-16 15:47:44 -0700

    Clean up the tests.

--- myproject/subdir1/class1.py
+++ myproject/subdir1/class1.py
@@ -1,23 +1,36 @@
 from myproject import get_logger
 
-class1_logger = get_logger()
+# Module level logger
+ml = get_logger()
+ml.info(f"Class1.Module level: The log file defaulted to 'class1' for its filename just as above.")
+ml.info("")
 
 
 class Class1:
-    logger = get_logger()
+    #Class level logger
+    cl = get_logger()
 
     def __init__(self):
-        Class1.logger.info(f"I am in {__name__} using the class  logger")
-        class1_logger.info(f"I am in {__name__} using the module logger")
-    
+        ml.info(        f"Init: Class1.Module level: The log file defaulted to 'class1'.")
+        self.cl.error(  f"Init: Class1.Clazz  level: The log file defaulted to 'class1'.")
+        self.cl.info("")
+
     def run(self):
-        Class1.logger.error(f"I am running in {__name__}.run() using the class  logger")
-        class1_logger.error(f"I am running in {__name__}.run() using the module logger")
+        rl = get_logger("clz1")
+        ml.info(        f"Run:  Class1.Module level: The log file defaulted to 'class1'.")
+        rl.info(        f"Run:  Class1.Clazz  level: The log file defaulted to 'clz1'.")
+        self.cl.warn(   f"Run:  Class1.Clazz  level: The log file defaulted to 'class1'.")
+        self.cl.info("")
 
 
 # Sample output.
 # --------------
-# 20230616Fri 140239.576 (0a0000E0.23596)[INFO class1] I am in myproject.subdir1.class1 using the class  logger
-# 20230616Fri 140239.576 (0a0000E0.23596)[INFO class1] I am in myproject.subdir1.class1 using the module logger
-# 0230616Fri 140239.577 (0a0000E0.23596)[ERR  class1] I am running in myproject.subdir1.class1.run() using the class  logger
-# 20230616Fri 140239.579 (0a0000E0.23596)[ERR  class1] I am running in myproject.subdir1.class1.run() using the module logger
+# 20230616Fri 154445.252 (0a0000E0.23484)[INFO class1] Class1.Module level: The log file defaulted to 'class1' for its filename just as above.
+# 20230616Fri 154445.253 (0a0000E0.23484)[INFO class1] 
+# 20230616Fri 154445.254 (0a0000E0.23484)[INFO class1] Init: Class1.Module level: The log file defaulted to 'class1'.
+# 20230616Fri 154445.254 (0a0000E0.23484)[ERR  class1] Init: Class1.Clazz  level: The log file defaulted to 'class1'.
+# 20230616Fri 154445.255 (0a0000E0.23484)[INFO class1] 
+# 20230616Fri 154445.256 (0a0000E0.23484)[INFO class1] Run:  Class1.Module level: The log file defaulted to 'class1'.
+# 20230616Fri 154445.256 (0a0000E0.23484)[INFO class1] Run:  Class1.Clazz  level: The log file defaulted to 'clz1'.
+# 20230616Fri 154445.257 (0a0000E0.23484)[WARN class1] Run:  Class1.Clazz  level: The log file defaulted to 'class1'.
+# 20230616Fri 154445.257 (0a0000E0.23484)[INFO class1]
--- myproject/subdir1/subdir2/class2.py
+++ myproject/subdir1/subdir2/class2.py
@@ -1,19 +1,24 @@
 from myproject import get_logger
 
-# class2_logger = get_logger()
-
-
 class Class2:
-    logger = get_logger()
+    #Class level logger
+    cl = get_logger()
 
     def __init__(self):
-        Class2.logger.info(f"I am in {__name__} using the class  logger")
-    
+        self.cl.error(  f"Init: Class2.Clazz  level: The log file defaulted to 'class2'.")
+        self.cl.info("")
+
     def run(self):
-        Class2.logger.warn(f"I am running in {__name__}.run() using the class  logger")
+        rl = get_logger("clz2")
+        rl.info(        f"Run:  Class2.Clazz  level: The log file defaulted to 'clz2'.")
+        self.cl.warn(   f"Run:  Class2.Clazz  level: The log file defaulted to 'class2'.")
+        self.cl.info("")
 
 
 # Sample output.
 # --------------
-# 20230616Fri 140239.581 (0a0000E0.23596)[INFO class2] I am in myproject.subdir1.subdir2.class2 using the class  logger
-# 20230616Fri 140239.582 (0a0000E0.23596)[WARN class2] I am running in myproject.subdir1.subdir2.class2.run() using the class  logger   
+# 20230616Fri 154445.261 (0a0000E0.23484)[ERR  class2] Init: Class2.Clazz  level: The log file defaulted to 'class2'.
+# 20230616Fri 154445.262 (0a0000E0.23484)[INFO class2]
+# 20230616Fri 154445.262 (0a0000E0.23484)[INFO class2] Run:  Class2.Clazz  level: The log file defaulted to 'clz2'.
+# 20230616Fri 154445.263 (0a0000E0.23484)[WARN class2] Run:  Class2.Clazz  level: The log file defaulted to 'class2'.
+# 20230616Fri 154445.263 (0a0000E0.23484)[INFO class2]
