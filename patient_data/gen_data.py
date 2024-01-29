a6ae9af68c27e53caf68a6d3774a9efe8f86a4ff
Nmeng01 <123509773+Nmeng01@users.noreply.github.com>
2024-01-29 14:37:35 -0500

    implemented dataclass and faker seed

--- patient_data/gen_data.py
+++ patient_data/gen_data.py
@@ -1,10 +1,78 @@
-from json_rest import Patient
+import random
+from faker import Faker
 from ujson import dumps
 from enum import Enum
 import sys
-from protobuf_grpc import generate_patients
+from datetime import date
+from dateutil.relativedelta import relativedelta
+import protobuf_grpc as proto
+import patient_dataclass as pd
+from dataclasses import asdict
+
+faker = Faker('en_US')
 
 
+# Method to create a fake name
+def generate_name(seed, birth_date, gender=''):
+    Faker.seed(seed)
+    if gender == 'Non-Binary' or gender == 'Other':
+        full_name = faker.name_nonbinary()
+    elif gender == 'Male':
+        full_name = faker.name_male()
+    elif gender == 'Female':
+        full_name = faker.name_female()
+    else:
+        full_name = faker.name()
+    n = pd.Name(
+        faker.word(ext_word_list=['usual', 'official', 'temp', 'nickname', 'anonymous', 'old', 'maiden']),
+        full_name,
+        full_name.split(' ')[0],
+        full_name.split(' ')[1],
+        faker.date_between(start_date=birth_date)
+    )
+
+    return n
+
+
+# Method to create a fake telecom
+def generate_telecom(seed, rank, birth_date, names):
+    Faker.seed(seed)
+    t = pd.Telecom(
+        faker.word(ext_word_list=['phone', 'fax', 'email', 'sms']),
+        faker.word(ext_word_list=['home', 'work', 'temp', 'old', 'mobile']),
+        rank,
+        faker.date_between(start_date=birth_date)
+    )
+    if t.system == 'email':
+        t.value = f'{names[random.randint(0, len(names) - 1)].given}' \
+                  f'{names[random.randint(0, len(names) - 1)].family}@{faker.domain_name()}'
+    else:
+        t.value = faker.phone_number()
+
+    return t
+
+
+# Method to create a fake address
+def generate_address(seed, birth_date):
+    Faker.seed(seed)
+    a = pd.Address(
+        faker.word(ext_word_list=['home', 'work', 'temp', 'old', 'billing']),
+        faker.word(ext_word_list=['postal', 'physical', 'both']),
+        faker.street_address(),
+        faker.city(),
+        faker.state(),
+        faker.postcode(),
+        'USA',
+        faker.date_between(start_date=birth_date),
+    )
+    a.text = f'{a.line} {a.city}, {a.state} {a.zipcode} {a.country}'
+    if a.use in ['temp', 'old']:
+        a.period_end = faker.date_between(start_date=a.period_start + relativedelta(years=1))
+
+    return a
+
+
+# Format class for ease of selecting data format
 class Format(Enum):
     Json = 1
     Protobuf = 2
@@ -17,21 +85,70 @@ class Format(Enum):
 
 
 def main(size, data_format: Format):
+    # Set-up
     curr_size = 0
+    seed = 1
     patient_list = []
+
+    # Set the maximum number of items for each repeated dataclass
+    max_names = 3
+    max_telecoms = 2
+    max_addresses = 2
+    max_contacts = 4
+
+    # Generate patients
     while curr_size < size:
-        p = Patient()
-        p.generate_names()
-        p.generate_telecoms()
-        p.generate_addresses()
-        p.generate_contacts()
+        Faker.seed(seed)
+        random.seed(seed)
+        p = pd.Patient(
+            faker.pyint(min_value=11111, max_value=99999),
+            faker.boolean(chance_of_getting_true=90),
+            faker.word(ext_word_list=['Male', 'Female', 'Non-binary', 'Other']),
+            faker.date_of_birth(minimum_age=18, maximum_age=100),
+            faker.word(ext_word_list=['Married', 'Single', 'Separated', 'Divorced', 'Widowed'])
+        )
+
+        # Find date of death if applicable
+        if p.birth_date < date.today() - relativedelta(years=65) and \
+           faker.boolean(chance_of_getting_true=15):
+            p.deceased_on = faker.date_between(start_date=date.today()-relativedelta(years=10)).strftime('%m/%d/%Y')
+
+        # Generate names
+        for x in range(random.randint(1, max_names)):
+            seed += 1
+            p.names.append(generate_name(seed, p.birth_date, p.gender))
+
+        # Generate telecoms
+        for x in range(random.randint(1, max_telecoms)):
+            seed += 1
+            p.telecoms.append(generate_telecom(seed, x+1, p.birth_date, p.names))
+
+        # Generate addresses
+        for x in range(random.randint(1, max_addresses)):
+            seed += 1
+            p.addresses.append(generate_address(seed, p.birth_date))
+        # Generate contacts
+        for x in range(random.randint(1, max_contacts)):
+            c_birth_date = faker.date_of_birth(minimum_age=18, maximum_age=100)
+            c = pd.Contact(
+                faker.word(ext_word_list=['emergency', 'family', 'guardian', 'friend', 'partner', 'work',
+                                          'caregiver', 'agent', 'guarantor', 'parent']),
+                generate_name(seed+1, c_birth_date),
+                generate_address(seed+1, c_birth_date)
+            )
+            seed += 3
+            c.telecom = generate_telecom(seed, 1, c_birth_date, [c.name])
+            p.contacts.append(c)
+        # Add patient to list and keep track of size
         patient_list.append(p)
         curr_size += sys.getsizeof(p)
+        seed += 1
+
     match data_format:
         case Format.Json:
-            return dumps([p.ret_dict() for p in patient_list])
+            return dumps([asdict(p) for p in patient_list], indent=4, default=str)
         case Format.Protobuf:
-            return generate_patients(size).all_patients
+            return proto.generate_patients(patient_list).all_patients
 
 
 if __name__ == '__main__':
--- patient_data/protobuf_grpc.py
+++ patient_data/protobuf_grpc.py
@@ -1,118 +1,80 @@
-import protobuf_grpc_pb2 as pb2
-from faker import Faker
 import datetime
-from dateutil.relativedelta import relativedelta
-import random
-import sys
+import protobuf_grpc_pb2 as pb2
+import patient_dataclass as pd
 
 
-def generate_name(full_name, birth_date):
-    faker = Faker('en_US')
+def generate_name(n: pd.Name):
     name = pb2.Name()
-    name.use = faker.word(ext_word_list=['usual', 'official', 'temp', 'nickname', 'anonymous', 'old', 'maiden'])
-    name.text = full_name
-    name.given = full_name.split(' ')[0]
-    name.family = full_name.split(' ')[1]
-    name.active = faker.date_between(start_date=datetime.datetime.strptime(birth_date, '%m/%d/%Y')).strftime('%m/%d/%Y')
+    name.use = n.use
+    name.text = n.text
+    name.given = n.given
+    name.family = n.family
+    name.active = n.active.strftime('%m/%d/%Y')
     return name
 
 
-def generate_telecom(birth_date, given, family):
-    faker = Faker('en_US')
+def generate_telecom(t: pd.Telecom):
     contact_point = pb2.Telecom()
-    contact_point.system = faker.word(ext_word_list=['phone', 'fax', 'email', 'sms'])
-    contact_point.use = faker.word(ext_word_list=['home', 'work', 'temp', 'old', 'mobile'])
-    contact_point.rank = 0  # set in gen_patient()
-    contact_point.active = faker.date_between(start_date=datetime.datetime.strptime(birth_date,
-                                              '%m/%d/%Y')).strftime('%m/%d/%Y')
-    if contact_point.system == 'email':
-        contact_point.value = f'{given}{family}@{faker.domain_name()}'
-    else:
-        contact_point.value = faker.phone_number()
+    contact_point.system = t.system
+    contact_point.use = t.use
+    contact_point.rank = t.rank
+    contact_point.active = t.active.strftime('%m/%d/%Y')
+    contact_point.value = t.value
     return contact_point
 
 
-def generate_address(birth_date):
-    faker = Faker('en_US')
+def generate_address(a: pd.Address):
     address = pb2.Address()
-    address.use = faker.word(ext_word_list=['home', 'work', 'temp', 'old', 'billing'])
-    address.type = faker.word(ext_word_list=['postal', 'physical', 'both'])
-    address.line = faker.street_address()
-    address.city = faker.city()
-    address.state = faker.word(ext_word_list=["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI",
-                                              "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI",
-                                              "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC",
-                                              "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT",
-                                              "VT", "VA", "WA", "WV", "WI", "WY"])
-    address.zipcode = faker.postcode()
-    address.country = 'USA'
-    address.period['start'] = faker.date_between(start_date=datetime.datetime.strptime(birth_date,
-                                                 '%m/%d/%Y')).strftime('%m/%d/%Y')
-    address.period['end'] = '' if address.use in ['home', 'work', 'billing'] else faker.date_between(
-                            start_date=datetime.datetime.strptime(address.period['start'], '%m/%d/%Y')
-                            + relativedelta(years=1)).strftime('%m/%d/%Y')
-    address.text = f'{address.line} {address.city}, {address.state} {address.zipcode} {address.country}'
+    address.use = a.use
+    address.type = a.type
+    address.line = a.line
+    address.city = a.city
+    address.state = a.state
+    address.zipcode = a.zipcode
+    address.country = a.country
+    address.period_start = a.period_start.strftime('%m/%d/%Y')
+    address.text = a.text
+    address.period_end = a.period_end.strftime('%m/%d/%Y') if isinstance(a.period_end, datetime.date) else ''
     return address
 
 
-def generate_contact():
-    faker = Faker('en_US')
+def generate_contact(c: pd.Contact):
     contact = pb2.Contact()
-    contact.relation = faker.word(ext_word_list=['emergency', 'family', 'guardian', 'friend', 'partner', 'work',
-                                                 'caregiver', 'agent', 'guarantor', 'parent'])
-    name = faker.name()
-    birth_date = faker.date_of_birth(minimum_age=18, maximum_age=100).strftime('%m/%d/%Y')
-    contact.name.CopyFrom(generate_name(name, birth_date))
-    contact.telecom.CopyFrom(generate_telecom(birth_date, name.split(' ')[0], name.split(' ')[1]))
-    contact.address.CopyFrom(generate_address(birth_date))
+    contact.relation = c.relation
+    contact.name.CopyFrom(generate_name(c.name))
+    contact.address.CopyFrom(generate_address(c.address))
+    contact.telecom.CopyFrom(generate_telecom(c.telecom))
     return contact
 
 
-def generate_patients(size):
-    curr_size = 0
+def generate_patient(p: pd.Patient):
+    patient = pb2.Patient()
+    patient.id = p.id
+    patient.active = p.active
+    patient.gender = p.gender
+    patient.birth_date = p.birth_date.strftime('%m/%d/%Y')
+    patient.deceased_on = p.deceased_on
+    patient.marital_status = p.marital_status
+
+    # Generate repeated Name objects
+    for n in p.names:
+        patient.names.append(generate_name(n))
+    # Generate repeated Telecom objects
+    for t in p.telecoms:
+        patient.telecoms.append(generate_telecom(t))
+    # Generate repeated Address objects
+    for a in p.addresses:
+        patient.addresses.append(generate_address(a))
+    # Generate repeated Contact objects
+    for c in p.contacts:
+        patient.contacts.append(generate_contact(c))
+
+    return patient
+
+
+def generate_patients(patient_list: list[pd.Patient]):
     patients = pb2.Patients()
-    while curr_size < size:
-        faker = Faker('en_US')
-        # Define preliminary patient attributes using faker
-        patient = pb2.Patient()
-        patient.id = faker.pyint(min_value=11111, max_value=99999)
-        patient.active = faker.boolean(chance_of_getting_true=90)
-        patient.gender = faker.word(ext_word_list=['Male', 'Female', 'Non-binary', 'Other'])
-        patient.birth_date = faker.date_of_birth(minimum_age=18, maximum_age=100).strftime('%m/%d/%Y')
-        patient.deceased_on = (faker.date_between(start_date=datetime.date.today()-relativedelta(years=10)).strftime(
-                               '%m/%d/%Y') if datetime.datetime.strptime(patient.birth_date, '%m/%d/%Y')
-                               < datetime.datetime.today()-relativedelta(years=65) and
-                               faker.boolean(chance_of_getting_true=15) else '')
-        patient.marital_status = faker.word(ext_word_list=['Married', 'Single', 'Separated', 'Divorced', 'Widowed'])
-
-        # Set the maximum number of items for each repeated object
-        max_names = 3
-        max_contact_points = 2
-        max_addresses = 2
-        max_contacts = 4
-
-        # Generate repeated Name objects
-        for x in range(random.randint(1, max_names)):
-            if patient.gender == 'Non-Binary' or patient.gender == 'Other':
-                full_name = faker.name_nonbinary()
-            else:
-                full_name = faker.name_male() if patient.gender == 'Male' else faker.name_female()
-            patient.names.append(generate_name(full_name, patient.birth_date))
-        # Generate repeated Telecom objects
-        for x in range(random.randint(1, max_contact_points)):
-            given = patient.names[random.randint(0, len(patient.names)-1)].given
-            family = patient.names[random.randint(0, len(patient.names)-1)].family
-            contact_point = generate_telecom(patient.birth_date, given, family)
-            contact_point.rank = x
-            patient.telecoms.append(contact_point)
-        # Generate repeated Address objects
-        for x in range(random.randint(1, max_addresses)):
-            patient.addresses.append(generate_address(patient.birth_date))
-        # Generate repeated Contact objects
-        for x in range(random.randint(1, max_contacts)):
-            patient.contacts.append(generate_contact())
-        # Update and keep track of size of patients
-        patients.all_patients.append(patient)
-        curr_size += sys.getsizeof(patient)
+    for p in patient_list:
+        patients.all_patients.append(generate_patient(p))
 
     return patients
