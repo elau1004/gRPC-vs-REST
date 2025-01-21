09de82cb66d6ea1e81095c9ff4b4f193b259e38c
Edward Lau <elau1004@netscape.net>
2025-01-20 16:17:51 -0800

    Added spooling to a log file.

--- restful_server.py
+++ restful_server.py
@@ -1,6 +1,7 @@
 """
 The RESTful server to serve out our test patient data.
 """
+import  time
 
 from    enum       import  Enum
 from    typing     import  List
@@ -23,26 +24,30 @@ app = FastAPI()
 
 @app.get("/")
 def index() -> str:
-    return 'Hello.  Server is running.  Parameters are: datasize: [full ,small ,medium ,large ,huge]'
+    return f'Hello.  {len(model.patients)} patients are ready.  Parameters are: datasize: [full ,small ,medium ,large ,huge]'
 
-@app.get("/patient/")
-def get_patients( runtoken: str = None ,compression: str = None ,datasize: str = None ) -> List[model.Patient]:
-    end: int = 1
-    match datasize: # The following sizes are obtained from inspecting the traffic in the browser.
-        case Size.FULL.value:   end = 1     # 1.1 kb
-        case Size.SMALL.value:  end = 5     # 9.9 kb
-        case Size.MEDIUM.value: end = 50    # 107 kb
-        case Size.LARGE.value:  end = 500   # 1.1 mb
-        case Size.HUGE.value:   end = 5000  # 11  mb
-        case _: end = 1
-
-    return  model.patients[0 : end]
-
-@app.get("/patient/{patient_id}")
+@app.get("/patients/{patient_id}")
 def get_patient( patient_id: int ) -> model.Patient:
     try:
         patient = model.patients[ patient_id ]
     except Exception as ex:
         # SEE:  https://www.websitepulse.com/kb/4xx_http_status_codes
         raise HTTPException(status_code=400, detail=f"patient with ID={patient_id=} doesn't exist.")
+
     return  patient
+
+@app.get("/patients/")
+def get_patients( runtoken: str = None ,datasize: str = None ) -> List[model.Patient]:
+    with open(f'logs/restful_server.log', 'a') as file:   # Python has a 8K buffer.
+        file.write(f'{time.time_ns():9}\t3 Server Rcv\t{runtoken}\n')
+        end: int = 1
+        match datasize: # The following sizes are obtained from inspecting the traffic in the browser.
+            case Size.FULL.value:   end = 1     # 1.1 kb
+            case Size.SMALL.value:  end = 5     # 9.9 kb
+            case Size.MEDIUM.value: end = 50    # 107 kb
+            case Size.LARGE.value:  end = 500   # 1.1 mb
+            case Size.HUGE.value:   end = 5000  # 11  mb
+            case _: end = 1
+
+        file.write(f'{time.time_ns():9}\t5 Server Rsp\t{runtoken}\n')
+    return  model.patients[0 : end]
